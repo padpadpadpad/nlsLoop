859bbf2e732ce6058542f623a18127ee4c10fd82
padpadpadpad <d.padfieldscfc@gmail.com>
2016-10-12 14:31:15 +0100

    adding colour to plot_all_nlsLoop

--- R/plot_all_nlsLoop.R
+++ R/plot_all_nlsLoop.R
@@ -6,6 +6,10 @@
 #' @param file_name the desired path for the pdf to be saved to.
 #' @param raw_data the data frame containing the data used in the nlsLoop argument
 #' @param param_data the nlsLoop object
+#' @param id_col an optional argument if a different column is desired from which each new plot panel is chosen
+#' @param col_point optional argument to control colour of points
+#' @param col_line optional argument to control colour of predictions
+#' @param group optional argument to ensure lines are grouped properly
 #' @author Daniel Padfield
 #' @return a plot using ggplot2 of the raw points and predictions of a single level of \code{id_col}
 #' @examples
@@ -27,23 +31,35 @@
 #' }
 #' @export plot_all_nlsLoop
 
-plot_all_nlsLoop <- function(file_name, raw_data, param_data){
-  # load in package
-  id_col <- as.character(param_data$info$id_col)
+plot_all_nlsLoop <- function(file_name, raw_data, param_data, id_col = NULL, col_point = NULL, col_line = NULL, group = NULL, ...){
+
+  # if statements if things are null
+  if(is.null(id_col)){id_col <- as.character(param_data$info$id_col)}
+  if(is.null(col_line)){param_data$predictions$col_line <- 'red'}
+  if(is.null(col_point)){raw_data$col_point <- 'black'}
+  if(is.null(group)){group <- col_line}
+
+
   x <- as.character(param_data$info$params_ind)
   y <- as.character(param_data$info$param_dep)
-  raw_data <- raw_data[,c(x,y, id_col)]
+  raw_data <- raw_data[,c(x, y, id_col, col_point)]
   raw_data <- raw_data[stats::complete.cases(raw_data),]
 
   predict_data <- param_data$predictions
 
   id <- unique(raw_data[,id_col])
 
-  grDevices::pdf(file_name)
+  ggplot2::update_geom_defaults("line", list(colour = 'red', linetype = 2))
+
+  grDevices::pdf(file_name, ...)
   for(i in 1:length(id)){
     plot <- ggplot2::ggplot() +
-      ggplot2::geom_line(ggplot2::aes_string(x = x, y = y), predict_data[predict_data[,id_col] == id[i],], col = 'red', linetype = 2) +
-      ggplot2::geom_point(ggplot2::aes_string(x = x, y = y), shape = 21, fill = 'white', size = 2.75, raw_data[raw_data[,id_col] == id[i],]) +
+      ggplot2::geom_line(ggplot2::aes_string(x = x, y = y,
+                                             col = col_line, group = group),
+                         predict_data[predict_data[,id_col] == id[i],], linetype = 2) +
+      ggplot2::geom_point(ggplot2::aes_string(x = x, y = y, col = col_point),
+                          shape = 21, fill = 'white', size = 2.75,
+                          raw_data[raw_data[,id_col] == id[i],]) +
       ggplot2::ylab(y) +
       ggplot2::xlab(x) +
       ggplot2::theme_bw(base_family = 'Helvetica', base_size = 14) +
